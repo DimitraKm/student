a088a0b8b12be05aa70f5502f2b430ae98588e33
DimitraKm <d.kontaki13@gmail.com>
2023-05-13 10:46:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            student s1 = new student("Moris", 15, 7648, 5, 6, 4, 6, 5);
+            student s2 = new student("Julian", 16, 8364, 4, 5, 6, 3, 6);
+            student s3 = new student("Mort", 15, 3764, 3, 5, 6, 4, 3);
+            student s4 = new student("Rico", 15, 2663, 4, 2, 5, 4, 6);
+            student s5 = new student("Rednic", 16, 9438, 4, 6, 6, 2, 3);
+
+            s1.print();
+            s2.print();
+            s3.print();
+            s4.print();
+            s5.print();
+
+        }
+    }
+}
---
+++ strudent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    internal class student
+    {
+
+        public string name; 
+        public int age; 
+        public int id;
+        public int grade_math;
+        public int grade_charms;
+        public int grade_potions;
+        public int grade_astronomy;
+        public int grade_herbology;
+        
+
+        public student(string name, int age, int id, int grade_math, int grade_charms, 
+            int grade_potions, int grade_astronomy, int grade_herbology)
+        {
+            this.name = name;
+            this.age = age;
+            this.id = id;
+            this.grade_math = grade_math;
+            this.grade_charms = grade_charms;
+            this.grade_potions = grade_potions;
+            this.grade_astronomy = grade_astronomy;
+            this.grade_herbology = grade_herbology;
+
+            int result = (grade_math + grade_charms + grade_potions + grade_astronomy + grade_herbology) / 5;
+
+            
+        }
+        public void print()
+        {
+            int result = (grade_math + grade_charms + grade_potions + grade_astronomy + grade_herbology) / 5;
+            Console.WriteLine(name + " " + result);
+            Console.WriteLine(name + " " + result).;
+        }
+        
+
+    }
+}
